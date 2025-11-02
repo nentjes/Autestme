e9a4901f25e4b1dc3cfa630d150b0a7f2794d89a
nentjes <roel@nentjes.nl>
2025-11-02 12:33:27 +0100

    Basis Watch App target toegevoegd

---
+++ AutestmeWatch Watch App/AutestmeWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutestmeWatchApp.swift
+//  AutestmeWatch Watch App
+//
+//  Created by Markus Moritz on 02/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AutestmeWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutestmeWatch Watch AppTests/AutestmeWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutestmeWatch_Watch_AppTests.swift
+//  AutestmeWatch Watch AppTests
+//
+//  Created by Markus Moritz on 02/11/2025.
+//
+
+import Testing
+@testable import AutestmeWatch_Watch_App
+
+struct AutestmeWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
